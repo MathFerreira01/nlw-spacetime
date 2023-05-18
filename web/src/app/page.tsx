1a4e8df00cf6cc9a10eae4699d0707e87586a9ec
mathferreira01 <ferreiramatheus124@gmail.com>
2023-05-17 22:54:54 -0300

    build: primeiro commit setup mobile

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="m-10 h-20 w-20 rounded bg-red-500 text-2xl font-bold uppercase">
-      Hello word
-    </h1>
+    <div className="h-screen bg-zinc-950">
+      <h1 className="text-4xl">Hello word</h1>
+    </div>
   )
 }
