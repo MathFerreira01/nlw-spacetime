df9b49fe582eda01825e85a1e57ef7ae5babb7de
mathferreira01 <ferreiramatheus124@gmail.com>
2023-05-17 22:22:01 -0300

    build: configura o prettier para o tailwind no setup web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <h1>Hello word</h1>
+    <h1 className="m-10 h-20 w-20 rounded bg-red-500 text-2xl font-bold uppercase">
+      Hello word
+    </h1>
   )
 }
