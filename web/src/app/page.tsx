94bb0a9c88578e8ac5d5246236f98c0c40fb7f66
mathferreira01 <ferreiramatheus124@gmail.com>
2023-05-17 23:22:39 -0300

    feat: adiciona as fontes no setup web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950">
-      <h1 className="text-4xl">Hello word</h1>
+    <div className="h-screen bg-zinc-950 ">
+      <h1 className="font-sans text-4xl">Hello word</h1>
+      <h1 className="font-alt text-4xl">Hello word</h1>
     </div>
   )
 }
