58d9083d7570c3e42d91125895fc058a7b87a769
mathferreira01 <ferreiramatheus124@gmail.com>
2023-05-20 01:59:16 -0300

    feat: cria rota de autenticação, token JWTe rotas autenticadas no setup server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,20 @@
 import fastify from 'fastify'
+import jwt from '@fastify/jwt'
+import 'dotenv/config'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
