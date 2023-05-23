f1d4c4840a6a85c0769baf18c8394bf95a773250
mathferreira01 <ferreiramatheus124@gmail.com>
2023-05-23 02:33:21 -0300

    feat: cria rota de upload de arquivos e servindo arquivos estáticos

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,20 @@ import fastify from 'fastify'
 import jwt from '@fastify/jwt'
 import 'dotenv/config'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -15,6 +25,7 @@ app.register(jwt, {
   secret: 'spacetime',
 })
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
